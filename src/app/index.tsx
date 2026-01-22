b14d1e98fca3ad3b417bd67219221e554823ed08
ismailbarka <ismailbarka82@gmail.com>
2026-01-22 19:48:33 +0100

    chore: setup husky and commitlint

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+interface HasName {
+  FirstName: string
+  LastName: string
+}
+
+interface HasAddress {
+  Address: string
+}
+
+type HasNameAndAddress = (HasName & HasAddress) | HasName
+
+const test: HasNameAndAddress = {
+  FirstName: 'ismail',
+  LastName: 'barka',
+  Address: 'istanbul',
+}
+
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your home!</Text>
+      <Text>Open up App.tsx to start working on your testtttt!</Text>
       <StatusBar style="auto" />
     </View>
   )
